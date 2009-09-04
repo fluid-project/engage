073085029f6a320982984b5768fd9c0c11bc66eb
Justin Obara <obarajustin@gmail.com>
2009-09-04 13:50:19 +0000

    ENGAGE-13, ENGAGE-49: Added comments to the js file and did some minor refactoring as a caught issues along the way.
    svn path=/incubator/engage-client/trunk/; revision=7870

--- components/browse/js/NavigationList.js
+++ components/browse/js/NavigationList.js
@@ -16,6 +16,15 @@ fluid_1_2 = fluid_1_2 || {};
 
 (function ($, fluid) {
     
+    /**
+     * Creates a render component for the component tree. The key can be any key that a componet tree would take and the value is what would be assigned to it.
+     * For example if you wanted to have node that just prints out "Hello World" you could set the key to "value" and the value to "Hello World"
+     * 
+     * @param {Object} id, the ID used by the component tree
+     * @param {Object} key, a key representing an entry in a renderer component
+     * @param {Object} value, the value assigned to the key
+     * @param {Object} classes, (optional) can add classes without having to specify the decorator key. 
+     */
     var treeNode = function (id, key, value, classes) {
         var obj = {ID: id};
         obj[key] = value;
@@ -29,22 +38,38 @@ fluid_1_2 = fluid_1_2 || {};
         return obj; 
     };
     
+    /**
+     * Creates a renderer component of type message
+     * 
+     * @param {Object} id, the ID used by the component tree
+     * @param {Object} messageKey, an EL path into the message bundle representing the template to be used
+     * @param {Object} messageArgs, the arguements to be merged into the message template
+     * @param {Object} classes, (optional) can be used to add classes 
+     */
     var compileMessage = function (id, messageKey, messageArgs, classes) {
         var obj = treeNode(id, "messagekey", messageKey, classes);
         obj.args = messageArgs;
         return obj;
     };
     
+    /**
+     * Will return the result of a function wich is run based on a condition.
+     * 
+     * @param {Object} condition, the test condition
+     * @param {Object} onTrue, the function to run if the condition passes
+     * @param {Object} onFalse, the function to run if the condition fails
+     */
     var conditionalNode = function (condition, onTrue, onFalse) {
         var func = condition ? onTrue : onFalse;
         
         return func();
     };
     
-    var addCount = function (string, count) {
-        return string + " (" + count + ")";
-    };
-    
+    /**
+     * Renders the copmonent based on values passed into the options
+     * 
+     * @param {Object} that, the component
+     */
     var render = function (that) {
          var selectorMap = [
             {selector: that.options.selectors.listItems, id: "listItems:"},
@@ -61,7 +86,7 @@ fluid_1_2 = fluid_1_2 || {};
                 var tree = treeNode("listItems:", "children", [
                     treeNode("link", "target", object.target || "", styles.link),
                     conditionalNode(object.category, function () {
-                        return compileMessage("titleText", "linkToMoreMessage", [addCount(object.category, object.size || "")], styles.category);
+                        return compileMessage("titleText", "linkToMoreMessage", [object.category || "", object.size || ""], styles.category);
                     }, function () {
                         return treeNode("titleText", "value", title, styles.titleText);
                     }),
@@ -98,25 +123,40 @@ fluid_1_2 = fluid_1_2 || {};
          
     };
     
+    /**
+     * The styles to be set on the group containing the list of links
+     * 
+     * @param {Object} that, the component
+     */
     var styleGroup = function (that) {
         that.locate("listGroup").addClass(that.options.styles.listGroup);
     };
     
+    /**
+     * The general setup function that calls the functions that need to be run on init
+     * 
+     * @param {Object} that, the component
+     */
     var setup = function (that) {
         render(that);
         styleGroup(that);
     };
     
+    /**
+     * The creator function
+     * 
+     * @param {Object} container, the components container
+     * @param {Object} options, the options passed into the component
+     */
     fluid.navigationList = function (container, options) {
         var that = fluid.initView("fluid.navigationList", container, options);
         
         setup(that);
     };
     
-    fluid.navigationList.calculateListSize = function (that) {
-        return that.model.links.length || 0;
-    };
-    
+    /**
+     * The components defaults
+     */
     fluid.defaults("fluid.navigationList", {
         selectors: {
             listGroup: ".flc-nagivationList-listGroup",
@@ -141,7 +181,7 @@ fluid_1_2 = fluid_1_2 || {};
         
         events: {},
         
-        messageBundle: {linkToMoreMessage: "See all in {0}"},
+        messageBundle: {linkToMoreMessage: "See all in {0} ({1})"},
         
         links: [
                 {
