78c75032f900e30d6c58497eea13efa04612082b
Svetoslav Nedkov <snedkov@asteasolutions.com>
2009-11-16 14:31:33 +0000

    ENGAGE-150: fixing the code for My Collections after review
    svn path=/scratchpad/myCollection-tryout/; revision=8693

--- fluid-engage-core/components/myCollection/js/myCollection.js
+++ fluid-engage-core/components/myCollection/js/myCollection.js
@@ -1,42 +1,42 @@
 /*
  Copyright 2009 University of Toronto
- 
+
  Licensed under the Educational Community License (ECL), Version 2.0 or the New
  BSD license. You may not use this file except in compliance with one these
  Licenses.
- 
+
  You may obtain a copy of the ECL 2.0 License and BSD License at
  https://source.fluidproject.org/svn/LICENSE.txt
- 
+
  */
 /*global jQuery, fluid*/
 
 fluid = fluid || {};
 
 (function ($) {
-	/**
-	* Creates a render component for the component tree. The key can be any key that a componet tree would take and the value is what would be assigned to it.
-	* For example if you wanted to have node that just prints out "Hello World" you could set the key to "value" and the value to "Hello World"
-	* 
-	* @param {Object} id, the ID used by the component tree
-	* @param {Object} key, a key representing an entry in a renderer component
-	* @param {Object} value, the value assigned to the key
-	* @param {Object} classes, (optional) can add classes without having to specify the decorator key. 
-	*/
-	var treeNode = function (id, key, value, classes) {
-	    var obj = {ID: id};
-	    obj[key] = value;
-	    if (classes) {
-			obj.decorators = {
-			    type: "addClass",
-			    classes: classes
-			};
-	    }
-	    
-	    return obj; 
-	};
-	
-	   
+    /**
+     * Creates a render component for the component tree. The key can be any key that a componet tree would take and the value is what would be assigned to it.
+     * For example if you wanted to have node that just prints out "Hello World" you could set the key to "value" and the value to "Hello World"
+     * 
+     * @param {Object} id, the ID used by the component tree
+     * @param {Object} key, a key representing an entry in a renderer component
+     * @param {Object} value, the value assigned to the key
+     * @param {Object} classes, (optional) can add classes without having to specify the decorator key. 
+     */
+    var treeNode = function (id, key, value, classes) {
+        var obj = {ID: id};
+        obj[key] = value;
+        if (classes) {
+            obj.decorators = {
+                    type: "addClass",
+                    classes: classes
+            	};
+        }
+
+        return obj; 
+    };
+
+
     /**
      * Will return the result of a function wich is run based on a condition.
      * 
@@ -46,23 +46,23 @@ fluid = fluid || {};
      */
     var conditionalNode = function (condition, onTrue, onFalse) {
         var func = condition ? onTrue : onFalse;
-        
+
         return func();
     };
-	
+
     var generateTree = function (that, componentOptions) {
         var styles = that.options.styles;
         return fluid.transform(componentOptions.links, function (object) {
             var title = object.title || "";
             var tree = treeNode("listItems:", "children", [
-                treeNode("link", "target", object.target || "", styles.link)
-            ], styles.listItems);
-            
+                                                           	treeNode("link", "target", object.target || "", styles.link),
+                                                           	], styles.listItems);
+
             if (styles.listGroup === "fl-list") {
-           		tree.children.push(treeNode("titleText", "value", title, styles.titleText));
+                tree.children.push(treeNode("titleText", "value", title, styles.titleText));
                 tree.children.push(treeNode("periodText", "value", object.dated, styles.periodText));
             }
-            
+
             if (object.image || that.options.useDefaultImage) {
                 tree.children.push({
                     ID: "image",
@@ -79,51 +79,6 @@ fluid = fluid || {};
     };
 
     /**
-     * Renders the copmonent based on values passed into the options
-     * 
-     * @param {Object} that, the component
-     */
-    var render = function (that) {
-        var selectorMap = [
-           {selector: that.options.selectors.listItems, id: "listItems:"},
-           {selector: that.options.selectors.link, id: "link"},
-           {selector: that.options.selectors.image, id: "image"},
-           {selector: that.options.selectors.titleText, id: "titleText"},
-           {selector: that.options.selectors.periodText, id: "periodText"}
-        ];
-    	
-		var componentOptions = {
-		    useDefaultImage: true
-		};
-
-        fluid.transform(that.locate("lists"), function (object, index) {
-            fluid.merge("merge", componentOptions, extractArray(that.options.lists, "listOptions")[index]);
-        });
-       
-        var options = {
-            cutpoints: selectorMap,
-            messageSource: {
-                type: "data"
-            }
-        };
-        
-        return fluid.selfRender(that.locate("listGroup"), generateTree(that, componentOptions), options);         
-    };
-    
-    /**
-     * The styles to be set on the group containing the list of links
-     * 
-     * @param {Object} that, the component
-     */
-    var styleGroup = function (that) {
-    	that.locate("listGroup").addClass(that.options.styles.listGroup);
-    };
-    
-    var styleToggler = function(that) {
-    	that.locate("toggler").addClass(that.options.styles.toggler);
-    }
-
-    /**
      * Traverses through an array of objects returning an array of all the values for a specified key.
      * 
      * @param {Object} array, an array of Objects to search through
@@ -137,6 +92,65 @@ fluid = fluid || {};
     };
 
     /**
+     * Renders the copmonent based on values passed into the options
+     * 
+     * @param {Object} that, the component
+     */
+    var render = function (that) {
+        var selectorMap = [
+                           {selector: that.options.selectors.listItems, id: "listItems:"},
+                           {selector: that.options.selectors.link, id: "link"},
+                           {selector: that.options.selectors.image, id: "image"},
+                           {selector: that.options.selectors.titleText, id: "titleText"},
+                           {selector: that.options.selectors.periodText, id: "periodText"}
+                           ];
+
+        var componentOptions = {
+                useDefaultImage: true
+        };
+
+        fluid.transform(that.locate("lists"), function (object, index) {
+            fluid.merge("merge", componentOptions, extractArray(that.options.lists, "listOptions")[index]);
+        });
+
+        if (that.templates) {
+            var resources = {
+                myCollection: {
+        			href: "myCollection.html", // find a way to avoid hardcoding this
+        			cutpoints: selectorMap
+        		}
+            };
+
+	        fluid.fetchResources(resources, function () {
+	            fluid.reRender(that.templates, that.locate("listGroup"), generateTree(that, componentOptions));
+	            that.events.afterRender.fire(that);
+	        });
+        } else {
+            var options = {
+                    cutpoints: selectorMap,
+                    messageSource: {
+            			type: "data"
+            		}    		
+            };
+        	
+        	return fluid.selfRender(that.locate("listGroup"), generateTree(that, componentOptions), options);         
+        }
+    };
+
+    /**
+     * The styles to be set on the group containing the list of links
+     * 
+     * @param {Object} that, the component
+     */
+    var styleGroup = function (that) {
+        that.locate("listGroup").addClass(that.options.styles.listGroup);
+    };
+
+    var styleToggler = function (that) {
+        that.locate("toggler").addClass(that.options.styles.toggler);
+    }
+
+    /**
      * Adds the loading style from the component, so that the loading message is displayed
      * 
      * @param {Object} that, the component
@@ -144,7 +158,7 @@ fluid = fluid || {};
     var addLoadStyling = function (that) {
         that.container.addClass(that.options.styles.load);
     };
-    
+
     /**
      * Removes the loading style from the component, so that the rendered page is displayed
      * 
@@ -153,7 +167,7 @@ fluid = fluid || {};
     var removeLoadStyling = function (that) {
         that.container.removeClass(that.options.styles.load);
     };
-    
+
     /**
      * Binds the after render event to a lister that calls the removeLoadStyling function
      * 
@@ -161,162 +175,130 @@ fluid = fluid || {};
      */
     var bindEvents = function (that) {
         that.events.afterRender.addListener(removeLoadStyling);
-        that.events.afterRender.addListener(initReorderer);
+    };
+
+    var addClickEvent = function (that) {
+        that.locate("toggler").click(that.toggleView);
+    };
+
+    var setup = function (that) {
+        that.templates = render(that);
+
+        styleGroup(that);
+        styleToggler(that);
+
+        addClickEvent(that);
+
+        bindEvents(that);
+        that.events.afterRender.fire(that);
+        
+        that.imageReorderer = fluid.initSubcomponent(that, "imageReorderer", [that.locate("myCollectionContainer"),
+                                                                              that.options.imageReorderer.options]);
     };
 
     fluid.initMyCollection = function (container, options) {
         var that = fluid.initView("fluid.initMyCollection", container, options);
-        
-        that.toggleView = function() {
-        	addLoadStyling(that);
-        	
-        	that.locate("listGroup").removeClass(that.options.styles.listGroup);
-        	
-        	if (that.options.styles.listGroup === "fl-grid") {
-        		that.options.styles.listGroup = "fl-list";
-        	} else {
-        		that.options.styles.listGroup = "fl-grid";
-        	}
-        	
-        	styleGroup(that);
 
-        	that.reRender();
-        }
-        
+        that.toggleView = function () {
+            addLoadStyling(that);
+
+            that.locate("listGroup").removeClass(that.options.styles.listGroup);
+
+            if (that.options.styles.listGroup === "fl-grid") {
+                that.options.styles.listGroup = "fl-list";
+            } else {
+                that.options.styles.listGroup = "fl-grid";
+            }
+
+            styleGroup(that);
+
+            render(that);
+        };
+
         setup(that);
 
         return that;
     };
-
-    var setup = function (that) {
-        var templates = render(that);
-        
-        that.reRender = function() {
-
-    		var componentOptions = {
-        		    useDefaultImage: true
-       		};
-
-            fluid.transform(that.locate("lists"), function (object, index) {
-                fluid.merge("merge", componentOptions, extractArray(that.options.lists, "listOptions")[index]);
-            });
-        	
-        	// Redundancy of selector map, have to get rid of it
-            var selectorMap = [
-               {selector: that.options.selectors.listItems, id: "listItems:"},
-               {selector: that.options.selectors.link, id: "link"},
-               {selector: that.options.selectors.image, id: "image"},
-               {selector: that.options.selectors.titleText, id: "titleText"},
-               {selector: that.options.selectors.periodText, id: "periodText"}
-            ];
-        	
-            var resources = {
-                myCollection: {
-                    href: "myCollection.html", // find a way to avoid hardcoding this
-                    cutpoints: selectorMap
-                }
-            };
-
-            fluid.fetchResources(resources, function () {
-            	fluid.reRender(templates, that.locate("listGroup"), generateTree(that, componentOptions));
-            	that.events.afterRender.fire(that);
-            });  
-        }
-        
-        styleGroup(that);
-        styleToggler(that);
-        
-        addClickEvent(that);
-        
-        bindEvents(that);
-        that.events.afterRender.fire(that);
-    }
-
-    var initReorderer = function() {
-
-		fluid.reorderImages(".flc-myCollection-imageContainer",
-			{
-				selectors: {
-					movables: ".flc-myCollection-movable",
-					dropTargets: ".flc-myCollection-movable"
-				},
-				styles: {
-				    defaultStyle: null,
-				    selected: null,
-				    dragging: null,
-				    mouseDrag: null,
-				    hover: null,
-				    dropMarker: null,
-				    avatar: null
-				}
-			}
-		);
-    }
     
-    var addClickEvent = function(that, template) {
-    	that.locate("toggler").click(that.toggleView);
-    }
+    var imageReordererOptions = {
+		selectors: {
+			movables: ".flc-myCollection-movable"
+		},
+	
+		styles: {
+			defaultStyle: null,
+	        selected: null,
+	        dragging: null,
+	        mouseDrag: null,
+	        hover: null,
+	        dropMarker: null,
+	        avatar: null
+		}
+	} 
     
-    fluid.defaults("fluid.initMyCollection", {
-        selectors: {
-            title: ".flc-myCollection-title",
-            myCollectionContents: ".flc-myCollection-contents",
-            lists: ".flc-myCollection-lists",
+    fluid.defaults("fluid.initMyCollection",
+    		{
+    			imageReorderer: {
+    				type: "fluid.reorderImages",
+    				options: imageReordererOptions
+    			},    			
+    		
+    			decorators: {
+        			type: "fluid",
+        			func: "fluid.reorderImages",
+        			container: ".flc-myCollection-imageContainer",
+        			options: imageReordererOptions
+        		},
+    			
+		        selectors: {
+    				myCollectionContainer: ".flc-myCollection-imageContainer",
+    				title: ".flc-myCollection-title",
+    				myCollectionContents: ".flc-myCollection-contents",
+			        lists: ".flc-myCollection-lists",			
+			        listGroup: ".flc-myCollection-listGroup",
+			        listItems: ".flc-myCollection-items",
+			        link: ".flc-myCollection-link",
+			        image: ".flc-myCollection-image",
+			        titleText: ".flc-myCollection-titleText",
+			        periodText: ".flc-myCollection-period",			
+			        toggler: ".flc-myCollection-toggler"
+		    	},
 
-            listGroup: ".flc-myCollection-listGroup",
-            listItems: ".flc-myCollection-items",
-            link: ".flc-myCollection-link",
-            image: ".flc-myCollection-image",
-            titleText: ".flc-myCollection-titleText",
-            periodText: ".flc-myCollection-period",
-            
-            toggler: ".flc-myCollection-toggler"
-        },
+		    	styles: {
+			        load: "fl-browse-loading",
+			        myCollectionContents: "fl-myCollection-contents",
+			        link: null,         
+			        listGroup: "fl-grid",
+			        titleText: null,
+			        periodText: null,			
+			        toggler: "fl-clickable"
+		    	},
 
-	 	styles: {
-	 		load: "fl-browse-loading",
-	   		myCollectionContents: "fl-myCollection-contents",
-			link: null,			
-			listGroup: "fl-grid",
-			titleText: null,
-			periodText: null,
+		    	events: {
+			        afterRender: null
+			    },
 
-			toggler: "fl-clickable"
-	 	},
+			    lists: [
+			            {
+			                category: "",
+			                description: "",
+			                listOptions: {}
+			            }
+			    ],
 
-        strings: {
-            title: "My Collection Title"
-        },
-        
-        events: {
-            afterRender: null
-        },
-        
-        useCabinet: false,
-        
-        componentTree: null,
-        selectorMap: null,
-
-        lists: [
-            {
-                category: "",
-                description: "",
-                listOptions: {}
-            }
-        ],
-
-        links: [
-                {
-                    target: "",
-                    image: "",
-                    title: "",
-                    dated: "",
-                    category: null,
-                    size: null
-                }
-            ],
-
-        useDefaultImage: true
-    });
+			    links: [
+			            {
+			                target: "",
+			                image: "",
+			                title: "",
+			                dated: "",
+			                category: null,
+			                size: null
+			            }
+			     ],
+			
+			     useDefaultImage: true
+    		}
+    );
 
 })(jQuery);
--- fluid-engage-kettle/src/main/webapp/services/myCollection/js/myCollection.js
+++ fluid-engage-kettle/src/main/webapp/services/myCollection/js/myCollection.js
@@ -18,96 +18,96 @@ fluid.myCollection = fluid.myCollection || {};
 
 (function ($) {
 
-	var parseEnv = function (envString, delimiter) {
-	    var obj = {};
-	    var ampIndex = envString.QUERY_STRING.indexOf(delimiter);
-	    
-	    obj.database = envString.QUERY_STRING.substring(0, ampIndex);
-	    obj.query = envString.QUERY_STRING.substring(ampIndex + 1, envString.QUERY_STRING.length);
-	    
-	    return obj;
-	};
-	
-	var compileDatabaseURL = function (parsedENV, config) {
-	    return fluid.stringTemplate(config.queryURLTemplate, 
+    var parseEnv = function (envString, delimiter) {
+        var obj = {};
+        var ampIndex = envString.QUERY_STRING.indexOf(delimiter);
+        
+        obj.database = envString.QUERY_STRING.substring(0, ampIndex);
+        obj.query = envString.QUERY_STRING.substring(ampIndex + 1, envString.QUERY_STRING.length);
+        
+        return obj;
+    };
+    
+    var compileDatabaseURL = function (parsedENV, config) {
+        return fluid.stringTemplate(config.queryURLTemplate, 
             {dbName: parsedENV.database || "", view: config.views.byCollectionCategory, query: parsedENV.query || ""});
-	};
-	
-	var compileTargetURL = function (URLBase, queryDelimiter, query, database) {
-	    return URLBase + (database || "") + queryDelimiter + query; 
-	};
-	
-	var compileData = function (data, dbName) {
-		var categoryText = (typeof data[0].category === "string") ? 
-				data[0].category : $.makeArray(data[0].category).toString();
-	    var model = {
-	        strings: {
-	            title: categoryText
-	        },
-	        useCabinet: false,
-	        lists: [{
-	            category: categoryText,
-	            listOptions: {}
-	        }]
-	    };
-	    
-	    model.lists[0].listOptions.links = fluid.transform(data, function (artifact) {
-	        return {
-		    target: "myCollection.html", // TODO - supply link to artifact page
-	            image: artifact.linkImage,
-	            title: artifact.linkTitle,
-	            dated: artifact.artifactDate
-	        };
-	    });
-	    
-	    return JSON.stringify(model);
-	};
-	
-	var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
-	    fluid.log("XMLHttpRequest: " + XMLHttpRequest);
-	    fluid.log("Status: " + textStatus);
-	    fluid.log("Error: " + errorThrown);
-	    return [500, {"Content-Type": "text/plain"}, errorThrown];
-	};
-	    
-	var ajaxCall = function (url, success, error) {
-	    $.ajax({
+    };
+    
+    var compileTargetURL = function (URLBase, queryDelimiter, query, database) {
+        return URLBase + (database || "") + queryDelimiter + query; 
+    };
+    
+    var compileData = function (data, dbName) {
+        var categoryText = (typeof data[0].category === "string") ? 
+                data[0].category : $.makeArray(data[0].category).toString();
+        var model = {
+            strings: {
+                title: categoryText
+            },
+            useCabinet: false,
+            lists: [{
+                category: categoryText,
+                listOptions: {}
+            }]
+        };
+        
+        model.lists[0].listOptions.links = fluid.transform(data, function (artifact) {
+            return {
+            target: "myCollection.html", // TODO - supply link to artifact page
+                image: artifact.linkImage,
+                title: artifact.linkTitle,
+                dated: artifact.artifactDate
+            };
+        });
+        
+        return JSON.stringify(model);
+    };
+    
+    var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
+        fluid.log("XMLHttpRequest: " + XMLHttpRequest);
+        fluid.log("Status: " + textStatus);
+        fluid.log("Error: " + errorThrown);
+        return [500, {"Content-Type": "text/plain"}, errorThrown];
+    };
+        
+    var ajaxCall = function (url, success, error) {
+        $.ajax({
             url: url,
             dataType: "json",
             asyn: false,
             success: success,
             error: error
-	    });
-	};
-	
-	var getAjax = function (url, error) {
-	    var data;
-	    var success = function (returnedData) {
-	        data = returnedData;
-	    };
-	    
-	    ajaxCall(url, success, error);
-	    
-	    return JSON.parse(data);
-	};
-	
-	var getArtifactData = function (rawData, database) {
-	    var dataRows = rawData.rows || [];
-	    return fluid.transform(dataRows, function (row) {
-	        var artifact = row.doc;
-	        return fluid.engage.mapModel(artifact, database);
-	    });
-	};
-	
-	var getData = function (error, parsedENV, config) {
-	    var url = config.myCollectionQueryURLTemplate;
-	    var rawData = getAjax(url, error);
-	    
-	    var dataSet = getArtifactData(rawData, "mmi");
+        });
+    };
+    
+    var getAjax = function (url, error) {
+        var data;
+        var success = function (returnedData) {
+            data = returnedData;
+        };
+        
+        ajaxCall(url, success, error);
+        
+        return JSON.parse(data);
+    };
+    
+    var getArtifactData = function (rawData, database) {
+        var dataRows = rawData.rows || [];
+        return fluid.transform(dataRows, function (row) {
+            var artifact = row.doc;
+            return fluid.engage.mapModel(artifact, database);
+        });
+    };
+    
+    var getData = function (error, parsedENV, config) {
+        var url = config.myCollectionQueryURLTemplate;
+        var rawData = getAjax(url, error);
+        
+        var dataSet = getArtifactData(rawData, "mmi");
 
-//	    return compileData(dataSet, "mmi");
-	    return compileData(dataSet, "");
-	};
+//      return compileData(dataSet, "mmi");
+        return compileData(dataSet, "");
+    };
 
 
     fluid.myCollection.initDataFeed = function (config, app) {
@@ -123,9 +123,9 @@ fluid.myCollection = fluid.myCollection || {};
         var handler = fluid.engage.mountRenderHandler({
             config: config,
             app: app,
-	    target: "artifacts/",
-	    source: "components/myCollection/html/",
-	    sourceMountRelative: "engage"
+        target: "artifacts/",
+        source: "components/myCollection/html/",
+        sourceMountRelative: "engage"
         });
         
         handler.registerProducer("myCollection", function (context, env) {
