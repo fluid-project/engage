a8cac5056ef574e8297901f160bd22d35ff5b276
Justin Obara <obarajustin@gmail.com>
2009-12-09 19:26:18 +0000

    ENGAGE-192: Unit tests for the paging functionality. Will need to continue to improve code coverage.
    svn path=/fluid/engage/fluid-engage-core/trunk/; revision=8842

--- tests/framework-tests/engageClientUtils/js/EngageClientUtilsTests.js
+++ tests/framework-tests/engageClientUtils/js/EngageClientUtilsTests.js
@@ -13,24 +13,51 @@ https://source.fluidproject.org/svn/LICENSE.txt
 
 (function ($) {  
 
-    function hasSameValues (obj1, obj2) {
+    function hasSameValues(obj1, obj2) {
         var result;
         
-        if (obj1.length === obj2.length){
+        if (obj1.length === obj2.length) {
             for (var key in obj1) {
                 if (obj1.hasOwnProperty(key)) {
-                    if(obj1[key] !== obj2[key]) {
+                    if (obj1[key] !== obj2[key]) {
                         return false;
                     }
                     result = true;
                 }
             }
         }
-        
         return result;
     }
-
-    function testCookies (name, value, message) {
+    
+    function testPaging(initTestFunc, otherTests, options) {
+        function testAccessor(url, callback, data) {
+            return initTestFunc ? initTestFunc(url, callback, data) : {};
+        }
+        var p = fluid.engage.paging(fluid.merge("merge", {dataAccessor: testAccessor}, options));
+        
+        if (otherTests) {
+            otherTests(p);
+        }
+    }
+    
+    function pagingAjaxAsserts(url, expectedData, actualData) {
+        jqUnit.assertTrue("URL is a string", typeof url === "string");
+        jqUnit.assertTrue("Data is an object", typeof actualData === "object");
+        jqUnit.assertEquals("Data value is correct", JSON.stringify(expectedData), JSON.stringify(actualData));
+    }
+    
+    function pagingNextTests(pager) {
+        pager.options.dataAccessor = function (url, callback, data) {
+            pagingAjaxAsserts(url, {limit: 20, skip: 0}, data);
+        };
+        
+        jqUnit.assertTrue("Has next", pager.hasNext());
+        jqUnit.assertFalse("Doesn't have previous", pager.hasPrevious());
+        
+        pager.next();
+    }
+    
+    function testCookies(name, value, message) {
         fluid.engage.setCookie(name, value);
         
         if (typeof value === "object") {
@@ -40,7 +67,7 @@ https://source.fluidproject.org/svn/LICENSE.txt
         }
     }
     
-    function engageClientUtilsTests () {
+    function engageClientUtilsTests() {
         var tests = jqUnit.testCase("EngageClientUtils Tests");
         
         tests.test("Cookies Test:", function () {
@@ -48,6 +75,36 @@ https://source.fluidproject.org/svn/LICENSE.txt
             testCookies("test", "value2", "Cookie reset and retrieved");
             testCookies("test2", "value3", "New Cookie saved and retrieved");
             testCookies("test3", {test3: "value4"}, "Cookie Value is an object");
+        });
+        
+        tests.test("Paging Test: Initial ajax call (caching on)", function () {
+            testPaging(function (url, callback, data) {
+                pagingAjaxAsserts(url, {limit: 20}, data);
+            });
+        });
+        
+        tests.test("Paging Test: Initial ajax call (caching off)", function () {
+            testPaging(function (url, callback, data) {
+                pagingAjaxAsserts(url, {limit: 1}, data);
+            }, null, {useCaching: false});
+        });
+        
+        tests.test("Paging Test: Changed maxPageSize", function () {
+            testPaging(function (url, callback, data) {
+                pagingAjaxAsserts(url, {limit: 6}, data);
+            }, null, {maxPageSize: 6});
+        });
+        
+        tests.test("Paging Test: Next with fixed data size", function () {
+            testPaging(function (url, callback) {
+                callback({});
+            }, pagingNextTests, {dataSetSize: 5, useCaching: false});
+        });
+        
+        tests.test("Paging Test: Next with el path to datasize", function () {
+            testPaging(function (url, callback) {
+                callback({size: 10});
+            }, pagingNextTests, {dataSetSize: "size", useCaching: false});
         });
     }
     
