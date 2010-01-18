919f8c9e0514d9926d1deef281864886cce0eda1
Michelle D'Souza <michelled33@gmail.com>
2010-01-18 20:57:08 +0000

    NOJIRA: More linting. Crockford seems to have changed JSLint to not complain about tabs vs. spaces. It now only complains if they're mixed together.
    svn path=/fluid/engage/fluid-engage-core/trunk/; revision=9024

--- framework/js/loadData.js
+++ framework/js/loadData.js
@@ -13,59 +13,59 @@ https://source.fluidproject.org/svn/LICENSE.txt
 "use strict";
 
 (function ($, fluid) {
-	
-	fluid.engage = fluid.engage || {};
+    
+    fluid.engage = fluid.engage || {};
 
-	// The default, last-ditch place to go look for testing data.
-	var localTestDataURL = "../data/demoData.json";
+    // The default, last-ditch place to go look for testing data.
+    var localTestDataURL = "../data/demoData.json";
 
-	var getFeedURL = function (currentUrl) {
-	    return String(currentUrl).replace(".html", ".json");
-	};
-		
-	// This function is now deprecated in favour of fluid.engage.initComponentWithDataURL()
-	fluid.engage.initComponentWithDataFeed = function (currentUrl, componentName, container) {
+    var getFeedURL = function (currentUrl) {
+        return String(currentUrl).replace(".html", ".json");
+    };
+        
+    // This function is now deprecated in favour of fluid.engage.initComponentWithDataURL()
+    fluid.engage.initComponentWithDataFeed = function (currentUrl, componentName, container) {
 
-		var initEngageComponent = function (options) {
-			fluid.invokeGlobalFunction(componentName, [container || "body", options]);
-		};
-		
-		var isFile = currentUrl.protocol === "file:";		
-		
-		$.ajax({
-			url: isFile ? localTestDataURL : getFeedURL(currentUrl),
-			success: initEngageComponent,
-			dataType: "json",
-			async: true
-		});
-	};
-	
-	/**
-	 * Initializes the named component, automatically fetching data from an associated data feed. 
-	 * This function runs asynchronously and does not directly return the component.
-	 * 
-	 * @param componentName the name of the component to instantiate
-	 * @param container the container for the component
-	 * @param options options for the component; note that the model option will be replaced with data if any is returned from the feed
-	 * @param feedURL an optional URL to a data feed; this will be automatically calculated from window.location if not specified
-	 */
-	fluid.engage.initComponentWithDataURL = function (componentName, container, options, feedURL) {
-		options = options || {};
-		if (!feedURL) {
-			var loc = window.location;
-			feedURL = loc.protocol === "file:" ? localTestDataURL : getFeedURL(loc);
-		}
-		
-		$.ajax({
-			url: feedURL,
-			dataType: "json",
-			success: function (data) {
-				if (data) {
-					options.model = data;
-				}
-				fluid.invokeGlobalFunction(componentName, [container, options]);
-			}
-		});
-	};
-	
+        var initEngageComponent = function (options) {
+            fluid.invokeGlobalFunction(componentName, [container || "body", options]);
+        };
+        
+        var isFile = currentUrl.protocol === "file:";        
+        
+        $.ajax({
+            url: isFile ? localTestDataURL : getFeedURL(currentUrl),
+            success: initEngageComponent,
+            dataType: "json",
+            async: true
+        });
+    };
+    
+    /**
+     * Initializes the named component, automatically fetching data from an associated data feed. 
+     * This function runs asynchronously and does not directly return the component.
+     * 
+     * @param componentName the name of the component to instantiate
+     * @param container the container for the component
+     * @param options options for the component; note that the model option will be replaced with data if any is returned from the feed
+     * @param feedURL an optional URL to a data feed; this will be automatically calculated from window.location if not specified
+     */
+    fluid.engage.initComponentWithDataURL = function (componentName, container, options, feedURL) {
+        options = options || {};
+        if (!feedURL) {
+            var loc = window.location;
+            feedURL = loc.protocol === "file:" ? localTestDataURL : getFeedURL(loc);
+        }
+        
+        $.ajax({
+            url: feedURL,
+            dataType: "json",
+            success: function (data) {
+                if (data) {
+                    options.model = data;
+                }
+                fluid.invokeGlobalFunction(componentName, [container, options]);
+            }
+        });
+    };
+    
 })(jQuery, fluid);
