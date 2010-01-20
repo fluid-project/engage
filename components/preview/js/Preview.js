4111a9808dd2795604b36073092feba17056888f
Justin Obara <obarajustin@gmail.com>
2010-01-20 16:50:27 +0000

    ENGAGE-241: committing the work on a preview component to show off some thumbnail previews, with captions. This could not be fully tested due to missing data in the data feed.
    svn path=/fluid/engage/fluid-engage-core/trunk/; revision=9057

---
+++ components/preview/js/Preview.js
@@ -0,0 +1,70 @@
+/*
+Copyright 2009-2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+/*global jQuery*/
+/*global fluid*/
+
+fluid = fluid || {};
+
+(function ($) {
+	
+	var renderPreview = function (container, options) {
+		var tree = {children: []};
+		$.each(options.model, function (index, value) {
+			tree.children.push({
+				ID: "previewItems:",
+				children: [
+					fluid.engage.renderUtils.uiBound("previewItemCaption", value.title),
+					fluid.engage.renderUtils.decoratedUIBound("previewItemLink", [{
+						attrs: {
+							href: value.target
+						}
+						}, {
+							type: "addClass",
+							classes: value.media ? options.styles.mediaIncluded : ""
+						}]),
+                    fluid.engage.renderUtils.attrDecoratedUIBound("previewItemImage", "src", value.thumbnail)
+				]
+			});
+		});
+		fluid.engage.renderUtils.createRendererFunction(container,
+			options.selectors, {
+				repeatingSelectors: ["previewItems"]
+			})(tree);
+	};
+	
+	var setup = function (that) {
+		if (that.options.model.length && that.options.model.length > 0) {
+			renderPreview(that.container, that.options);
+		}
+		else {
+			that.container.parent().remove();
+		}
+	}; 
+	
+	fluid.engage.preview = function (container, options) {
+		var that = fluid.initView("fluid.engage.preview", container, options);
+		setup(that);
+		return that;
+	};
+
+    fluid.defaults("fluid.engage.preview", {
+		selectors: {
+			previewItems: ".flc-preview-items",
+            previewItemLink: ".flc-preview-item-link",
+            previewItemImage: ".flc-preview-item-image",
+            previewItemCaption: ".flc-preview-item-caption"
+		},
+		styles: {
+			mediaIncluded: "fl-preview-media-included"
+		}
+	});
+}(jQuery));
