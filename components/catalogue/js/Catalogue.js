9ceb2800bad199853e62b8f86e7588c2dfb93d60
Antranig Basman <antranig.basman@colorado.edu>
2010-01-22 02:16:27 +0000

    ENGAGE-263: Tabs to spaces
    svn path=/fluid/engage/fluid-engage-core/trunk/; revision=9098

--- components/catalogue/js/Catalogue.js
+++ components/catalogue/js/Catalogue.js
@@ -15,53 +15,53 @@ https://source.fluidproject.org/svn/LICENSE.txt
 fluid = fluid || {};
 
 (function ($) {
-	
-	var buildCutpoints = function (selectors) {
-		return [
+    
+    var buildCutpoints = function (selectors) {
+        return [
             {id: "exhibitionTitle", selector: selectors.exhibitionTitle}
         ];
-	};
-	
-	var buildComponentTree = function (that) {
-		return {
+    };
+    
+    var buildComponentTree = function (that) {
+        return {
             children: [{
                 ID: "exhibitionTitle",
                 value: that.model.strings.title
             }]
         };
-	};
-	
-	var renderCatalogue = function (that) {
-		fluid.selfRender(that.container, 
-			buildComponentTree(that), 
-			{cutpoints: buildCutpoints(that.options.selectors), model: that.model, debug: true});
-	};
-	
-	var initSubcomponents = function (that) {
-		that.catalogueList = fluid.initSubcomponent(that, "navigationList", [that.locate("catalogueList"),
-		    {links: that.model.lists[0].listOptions.links}]);
-	};
-	
-	var setup = function (that) {
-		renderCatalogue(that);
-		initSubcomponents(that);
-	};
-	
-	fluid.catalogue = function (container, options) {
-		var that = fluid.initView("fluid.catalogue", container, options);		
-		that.model = that.options.model;
-		setup(that);
-		return that;
-	};
-	
-	fluid.defaults("fluid.catalogue", {
-		selectors: {
-			exhibitionTitle: ".flc-exhibition-title",
-			viewAll: ".flc-catalogue-viewAll",
-			catalogueList: ".flc-catalogueList"
-		},
-		navigationList: {
+    };
+    
+    var renderCatalogue = function (that) {
+        fluid.selfRender(that.container, 
+            buildComponentTree(that), 
+            {cutpoints: buildCutpoints(that.options.selectors), model: that.model, debug: true});
+    };
+    
+    var initSubcomponents = function (that) {
+        that.catalogueList = fluid.initSubcomponent(that, "navigationList", [that.locate("catalogueList"),
+            {links: that.model.lists[0].listOptions.links}]);
+    };
+    
+    var setup = function (that) {
+        renderCatalogue(that);
+        initSubcomponents(that);
+    };
+    
+    fluid.catalogue = function (container, options) {
+        var that = fluid.initView("fluid.catalogue", container, options);        
+        that.model = that.options.model;
+        setup(that);
+        return that;
+    };
+    
+    fluid.defaults("fluid.catalogue", {
+        selectors: {
+            exhibitionTitle: ".flc-exhibition-title",
+            viewAll: ".flc-catalogue-viewAll",
+            catalogueList: ".flc-catalogueList"
+        },
+        navigationList: {
             type: "fluid.navigationList"
         }
-	});
+    });
 }(jQuery));
