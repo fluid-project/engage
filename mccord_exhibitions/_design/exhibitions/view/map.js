1682b7386acccfd772b00071fc8683e8ebe7eae6
Yura Zenevich <yzenevich@ocad.ca>
2010-02-02 17:10:42 +0000

    ENGAGE-289: Updated exhibition view to support catalogue preview.
    svn path=/scratchpad/couch-views/; revision=9228

--- mccord_exhibitions/_design/exhibitions/view/map.js
+++ mccord_exhibitions/_design/exhibitions/view/map.js
@@ -3,12 +3,14 @@ function (doc) {
         'title': doc.exhibit.title,
         'lang': doc.exhibit.lang
     }, {
+        'shortDescription': doc.exhibit.blurb || "",
         'endDate': doc.exhibit.enddate, 
         'isCurrent': doc.exhibit.iscurrent, 
         'displayDate': doc.exhibit.displaydate, 
         'image': doc.exhibit.images.image, 
         'content': doc.exhibit.content, 
         'introduction': doc.exhibit.introduction, 
-        'catalogueSize': doc.exhibit.artifacts ? doc.exhibit.artifacts.cnt : 0
+        'catalogueSize': doc.exhibit.artifacts ? doc.exhibit.artifacts.cnt : 0,
+        'cataloguePreview': doc.exhibit.highlights ? doc.exhibit.highlights.artifact : [] 
     });
 }
