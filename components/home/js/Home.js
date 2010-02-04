942ac2fe7dd4b88552aaaecc61d20c1ab0fb98d6
Justin Obara <obarajustin@gmail.com>
2010-02-04 19:02:33 +0000

    ENGAGE-284, ENGAGE-283: Componentized home.js, which handles the switching to the language selection and the rendering of the text based on a strings object in the options. Also added unit tests, which are all passing. Could still use some more refactoring, and the service/spout needs to be updated to look for and pass along a message bundle. One more thing is that the links need to be updated to pass along the lang param that is set by the language selection.
    svn path=/fluid/engage/fluid-engage-core/trunk/; revision=9269

--- components/home/js/Home.js
+++ components/home/js/Home.js
@@ -17,10 +17,82 @@ fluid = fluid || {};
     
     fluid.engage = fluid.engage || {};
     
-    fluid.engage.home = function () {
-        $(".flc-engage-home-language").click(function () {
-            $(".flc-engage-home").addClass("fl-hidden");
-            $(".flc-engage-languageSelection").removeClass("fl-hidden");
+    function makeProtoComponents() {
+        return {
+            homeTitle: {messagekey: "homeTitle"},
+            languageSelectionTitle: {messagekey: "languageSelectionTitle"},
+            exhibitionsCaption: {messagekey: "exhibitionsCaption"},
+            collectionsCaption: {messagekey: "collectionsCaption"},
+            myCollectionCaption: {messagekey: "myCollectionCaption"},
+            objectCodeCaption: {messagekey: "objectCodeCaption"},
+            languageCaption: {messagekey: "languageCaption"}
+        };
+    }
+    
+    function bindEvents(that) {
+        that.locate("languageLink").click(that.showLanguageSelection);
+    }
+    
+    function setup(that) {
+        var messageLocator = fluid.messageLocator(that.options.strings, fluid.stringTemplate);
+        var selectorsToIgnore = ["homeContent", "languageSelectionContent", "languageLink"];
+        that.render = fluid.engage.renderUtils.createRendererFunction(that.container, that.options.selectors, {
+            selectorsToIgnore: selectorsToIgnore,
+            rendererOptions: {
+                messageLocator: messageLocator
+            }
         });
+        that.refreshView();
+    }
+    
+    fluid.engage.home = function (container, options) {
+        var that = fluid.initView("fluid.engage.home", container, options);
+        var expander = fluid.renderer.makeProtoExpander({ELstyle: "%"});
+        
+        that.showLanguageSelection = function () {
+            var hidden = that.options.styles.hidden;
+            that.locate("homeContent").addClass(hidden);
+            that.locate("languageSelectionContent").removeClass(hidden);
+        };
+        
+        that.refreshView = function () {
+            var protoTree = makeProtoComponents();
+            var tree = expander(protoTree);
+            that.render(tree);
+            bindEvents(that);
+        };
+        
+        setup(that);
+        
+        return that;
     };
+    
+    fluid.defaults("fluid.engage.home", {
+        selectors: {
+            homeContent: ".flc-engage-home",
+            languageSelectionContent: ".flc-engage-languageSelection",
+            homeTitle: ".flc-engage-homeTitle",
+            languageSelectionTitle: ".flc-engage-languageSelectionTitle",
+            exhibitionsCaption: ".flc-engage-homeExhibitionsCaption",
+            collectionsCaption: ".flc-engage-homeCollectionsCaption",
+            myCollectionCaption: ".flc-engage-homeMyCollectionCaption",
+            objectCodeCaption: ".flc-engage-homeObjectCodeCaption",
+            languageCaption: ".flc-engage-homeLanguageCaption",
+            languageLink: ".flc-engage-home-language"
+        },
+        
+        styles: {
+            hidden: "fl-hidden"
+        },
+        
+        strings: {
+            exhibitionsCaption: "Exhibitions",
+            collectionsCaption: "Collections",
+            myCollectionCaption: "My collection",
+            objectCodeCaption: "Enter object code",
+            languageCaption: "Change language",
+            homeTitle: "McCord Museum",
+            languageSelectionTitle: "Language Selection"
+        }
+    });
 })(jQuery);
---
+++ tests/component-tests/home/js/HomeTests.js
@@ -0,0 +1,60 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+ 
+*/
+/*global jQuery*/
+/*global fluid*/
+/*global jqUnit expect stop start*/
+
+(function ($) {
+    
+    var selector = ".flc-engage-homeAndLanguage";
+    var component;
+    
+    var homeTests = jqUnit.testCase("Home Screen Tests", function () {
+        homeTests.fetchTemplate("../../../../components/home/html/home.html", selector);
+        component = fluid.engage.home(selector);
+    });
+    
+    function stylingTests(styled, unStyled, className) {
+        jqUnit.assertTrue("Has class: " + className, $(styled).hasClass(className));
+        jqUnit.assertFalse("Does not have class: " + className, $(unStyled).hasClass(className));
+    }
+    
+    function testStringApplication(strings, selectors) {
+        for (var key in strings) {
+            var string = strings[key];
+            jqUnit.assertEquals("Ensure correct text applied", string, $(selectors[key]).text());
+        }
+    }
+    
+    function tests() {
+        homeTests.test("Page load styling", function () {
+            var sel = component.options.selectors;
+            
+            stylingTests(sel.languageSelectionContent, sel.homeContent, component.options.styles.hidden);
+        });
+        
+        homeTests.test("Language Selection page stlying", function () {
+            var sel = component.options.selectors;
+            
+            component.showLanguageSelection();
+            stylingTests(sel.homeContent, sel.languageSelectionContent, component.options.styles.hidden);
+        });
+        
+        homeTests.test("Strings applied", function () {
+            testStringApplication(component.options.strings, component.options.selectors);
+        });
+    }
+    
+    $(document).ready(function () {
+        tests();
+    });
+})(jQuery);
