2f38134b290ec2b0b911f72b67e648e926accfb6
Antranig Basman <antranig.basman@colorado.edu>
2010-02-19 01:33:38 +0000

    ENGAGE-357: Updated deleted documents view, and refreshes guestbook page on action.
    svn path=/scratchpad/couch-views/; revision=9449

--- mccord_comments/_design/comments/views.js
+++ mccord_comments/_design/comments/views.js
@@ -2,7 +2,7 @@ var views = // Paste into Futon starting on line following
 { 
     "comments": {
         "map": "function(doc) {
-            if (doc.type === 'fluid.guestbook.comment') {
+            if (doc.type === 'fluid.guestbook.comment' && !doc.deleted) {
                 emit({type: doc.targetType, id: doc.targetId, date: doc.date}, doc);
             }
         }"
